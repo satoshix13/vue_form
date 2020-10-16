2159803390eb52c5b75f65b1c24b78041adf9767
satoshix13 <satoshix13@gmail.com>
2020-10-16 11:10:49 +0200

    required added

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,13 +1,13 @@
 <template>
    <div id="container">
-      <div class="container" :class="{ successMsg: success }">
+      <div class="container">
         <form action="post" @submit.prevent="submitHandler">
           <div class="row">
             <div class="col-25">
               <label for="fname">Фамилия*</label>
             </div>
             <div class="col-75 test">
-              <input v-model="form.familyName"
+              <input v-model.trim="form.familyName"
                 type="text"
                 id="fname"
                 name="firstname"
@@ -25,8 +25,10 @@
                 type="text"
                 id="lname"
                 name="lastname"
-                required
+                v-model.trim="form.name"
+                :class="$v.form.name.$error ? 'is-invalid' : '' "
               />
+               <p v-if="$v.form.familyName.$dirty && !$v.form.name.required" class="feedback">это поле обязательное</p>
             </div>
           </div>
           <div class="row">
@@ -53,8 +55,10 @@
                 id="bday"
                 name="bday"
                 placeholder="12.09.1992"
-               
+                :class="$v.form.bday.$error ?  'is-invalid' : ''"
+                v-model.trim="form.bday"
               />
+               <p v-if="$v.form.bday.$dirty && !$v.form.bday.required" class="feedback">это поле обязательное</p>
             </div>
           </div>
           <div class="row">
@@ -69,8 +73,8 @@
                 placeholder="7"
                 :class="$v.form.tel.$error ?  'is-invalid' : ''"
               />
-              <p v-if="$v.form.tel.$dirty && !$v.form.tel.elevenDigits">Должно быть 11 цифр</p>
-              <p v-if="$v.form.tel.$dirty && !$v.form.tel.startsWith">Номер должен начинаться с 7</p>
+              <p v-if="$v.form.tel.$dirty && !$v.form.tel.elevenDigits" class="feedback">Должно быть 11 цифр</p>
+              <p v-if="$v.form.tel.$dirty && !$v.form.tel.startsWith" class="feedback">Номер должен начинаться с 7</p>
             </div>
           </div>
           <div class="row">
@@ -85,7 +89,7 @@
               />
             </div>
           </div>
-          <!-- <div class="row">
+          <div class="row">
             <div class="col-25">
               <label for="сlients">Группа клиентов*</label>
             </div>
@@ -96,15 +100,208 @@
                 <option value="oms">ОМС</option>
               </select>
             </div>
+          </div>
+           <!-- <div class="row">
+            <div class="col-25">
+              <label for="сlients">Группа клиентов*</label>
+            </div>
+            <div class="col-75">
+              <div class="form-check">
+                <input type="checkbox" name="selectednames[]" value = "VIp" />
+                <input type="checkbox" name="selectednames[]" value = "2" />
+                <input type="checkbox" name="selectednames[]" value = "3" />
+                <input type="checkbox" name="selectednames[]" value = "4" />
+            </div>
+            </div>
           </div> -->
 
+
+
+          <div class="row">
+            <div class="col-25">
+              <label for="doctor">Лечащий врач</label>
+            </div>
+            <div class="col-75">
+              <select id="doctor" name="doctor">
+                <option value="Ivanov">Иванов</option>
+                <option value="Zaharov">Захаров</option>
+                <option value="Chernishev">Чернышева</option>
+              </select>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-25">
+              <label for="sms">смс</label>
+            </div>
+            <div class="col-75">
+              <div>
+                <input type="checkbox" id="sms" name="sms"
+                       checked>
+                <label for="sms">Не отправлять</label>
+              </div>
+            </div>
+          </div>
+
+          <hr />
+
+            <div class="row">
+            <div class="col-25">
+              <label for="index">Индекс</label>
+            </div>
+            <div class="col-75">
+              <input
+                type="number"
+                id="index"
+                name="index"
+
+              />
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-25">
+              <label for="country">Страна</label>
+            </div>
+            <div class="col-75">
+              <input
+                type="text"
+                id="country"
+                name="country"
+              />
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-25">
+              <label for="region">Область</label>
+            </div>
+            <div class="col-75">
+              <input
+                type="text"
+                id="region"
+                name="region"
+      
+              />
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-25">
+              <label for="city">Город*</label>
+            </div>
+            <div class="col-75">
+              <input
+                type="text"
+                id="city"
+                name="city"
+                v-model.trim="form.city"
+                :class="$v.form.city.$error ?  'is-invalid' : ''"
+              />
+              <p v-if="$v.form.city.$dirty && !$v.form.city.required" class="feedback">это поле обязательное</p>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-25">
+              <label for="street">Улица</label>
+            </div>
+            <div class="col-75">
+              <input
+                type="text"
+                id="street"
+                name="street"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-25">
+              <label for="house">Дом</label>
+            </div>
+            <div class="col-75">
+              <input
+                type="text"
+                id="house"
+                name="house"
+              />
+            </div>
+          </div>
+
+          <hr />
         
+        <div class="row">
+            <div class="col-25">
+              <label for="document">Тип документа*</label>
+            </div>
+            <div class="col-75">
+              <select id="document" name="document" required>
+                <option value="pas">Паспорт</option>
+                <option value="certificate">Свидетельство о Рождении</option>
+                <option value="license">Вод. удостоверение</option>
+              </select>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-25">
+              <label for="seria">Серия</label>
+            </div>
+            <div class="col-75">
+              <input
+                type="number"
+                id="seria"
+                name="seria"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-25">
+              <label for="pasnumber">Номер</label>
+            </div>
+            <div class="col-75">
+              <input
+                type="number"
+                id="pasnumber"
+                name="pasnumber"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-25">
+              <label for="issuedby">Кем выдан</label>
+            </div>
+            <div class="col-75">
+              <input
+                type="text"
+                id="issuedby"
+                name="issuedby"
+              />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-25">
+              <label for="issuedate">Дата выдачи*</label>
+            </div>
+            <div class="col-75">
+              <input
+                type="text"
+                id="issuedate"
+                name="issuedate"
+                placeholder="12.09.2010"
+                v-model.trim="form.issuedate"
+                :class="$v.form.issuedate.$error ?  'is-invalid' : ''"
+              />
+              <p v-if="$v.form.issuedate.$dirty && !$v.form.issuedate.required" class="feedback">это поле обязательное</p>
+            </div>
+          </div>
+
           <div class="row">
             <div class="col-25">
               <label>*Поле обязательное для заполнения.</label>
             </div>
             <input type="submit" value="Отправить" />
           </div>
+          <div v-if="success" class="successMsg">
+                <h2>Форма была успешна отправлена</h2>
+            </div>
         </form>
       </div>
     </div>
@@ -121,7 +318,11 @@ export default {
     return {
       form: {
         tel: "",
+        name:"",
+        bday:"",
         familyName: "",
+        city: "",
+        issuedate: "",
       },
       success: false,
     }
@@ -129,7 +330,11 @@ export default {
   validations: {
      form: {
        familyName: { required },
-       tel: { elevenDigits, startsWith }
+       name: { required },
+       bday: { required },
+       city: { required },
+       tel: { elevenDigits, startsWith },
+       issuedate: { required }
      }
   },
   methods: {
@@ -146,7 +351,11 @@ export default {
 <style scoped>
 
 .successMsg {
-   display: none;
+  color: green;
+  margin-top: 20px;
+  border: 3px solid;
+  border-color: green;
+  display: inline-block;
 }
 
 .is-invalid {
